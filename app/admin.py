f9af258c62a1c3d9116ca4a939e2c80449704cae
tomohito <dveloptomohito@gmail.com>
2016-01-08 20:47:15 +0900

    auth function

--- app/admin.py
+++ app/admin.py
@@ -2,12 +2,19 @@
 
 from django.contrib import admin
 from app.models.model_android import BasicAndroid
+from app.models.model_auth_token import UserModel
 from app.models.model_java import BasicJava
 from app.models.model_python import BasicPython
 
 
-@admin.register(BasicJava)
+@admin.register(UserModel)
 class UserAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ('name', 'password')
+    # list_display_links = ('name', 'password',)
+
+@admin.register(BasicJava)
+class JavaAdmin(admin.ModelAdmin):
     pass
 
 
--- app/models/model_auth_token.py
+++ app/models/model_auth_token.py
@@ -4,10 +4,13 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django import forms
 from rest_framework.authtoken.models import Token
 
 
 class UserModel(models.Model):
+
+    name = models.CharField(max_length=80)
 
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def create_auth_token(sender, instance=None, created=False, **kwargs):
@@ -21,3 +24,4 @@ class UserModel(models.Model):
         """
         if created:
             Token.objects.create(user=instance)
+
