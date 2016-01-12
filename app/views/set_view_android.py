afc93b60f9616f26e42f6b42b8d5326094e2772a
tomohito <dveloptomohito@gmail.com>
2016-01-12 18:18:24 +0900

    add get and post method to androidsetview and pythonsetview

--- app/views/set_view_android.py
+++ app/views/set_view_android.py
@@ -11,3 +11,31 @@ class BasicViewSetAndroid(viewsets.ModelViewSet):
     """
     queryset = BasicAndroid.objects.all()
     serializer_class = BasicSerializerAndroid
+
+
+    @api_view(['GET', 'POST'])
+    def get(self, request, format=None):
+        """
+        GET method
+        :param request:
+        :param format:
+        :return:
+        """
+        java_basic = BasicAndroid.objects.all()
+        serializer = BasicSerializerAndroid(java_basic, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        """
+        POST method
+        :param request:
+        :param format:
+        :return:
+        """
+        serializer = BasicSerializerAndroid(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- app/views/set_view_python.py
+++ app/views/set_view_python.py
@@ -1,5 +1,9 @@
 # --*-- coding: utf-8 --*--
+
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from app.models.model_python import BasicPython
 from app.serializers.serializer_python import BasicSerializerPython
 
@@ -11,3 +15,32 @@ class BasicViewSetPython(viewsets.ModelViewSet):
     """
     queryset = BasicPython.objects.all()
     serializer_class = BasicSerializerPython
+
+
+    @api_view(['GET', 'POST'])
+    def get(self, request, format=None):
+        """
+        GET method
+        :param request:
+        :param format:
+        :return:
+        """
+        java_basic = BasicPython.objects.all()
+        serializer = BasicSerializerPython(java_basic, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request, format=None):
+        """
+        POST method
+        :param request:
+        :param format:
+        :return:
+        """
+        serializer = BasicSerializerPython(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
