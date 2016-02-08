c836c9d85425c7d507f7d2e8f4c60ab41212ee26
tomohito <dveloptomohito@gmail.com>
2016-02-08 17:55:24 +0900

    heroku

--- restful_api/wsgi.py
+++ restful_api/wsgi.py
@@ -11,6 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restful_api.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restful_api.settings")
+#
+# application = get_wsgi_application()
 
-application = get_wsgi_application()
+
+import os
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+
+application = Cling(get_wsgi_application())
